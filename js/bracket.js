270104d3612727fbbcb28464018fcf07503c87ad
sunoru <sunoru@live.com>
2015-07-08 22:10:40 +0800

    init

---
+++ js/bracket.js
@@ -0,0 +1,41 @@
+var mc = null;
+var data = {};
+var q = 0;
+function checkLoaded() {
+    if (q != 5)
+        return;
+    alert(q);
+}
+function loadData() {
+    var data_url = "data/";
+    var data_names = ["t1", "t2", "t3", "t4"];
+    $.get(data_url + "main.csv",function(data,status){
+        var data_tmp = new CSV(data, {
+            header: ["group_id", "signup_id", "baidu_id", "player_id", "friend_code"]
+        }).parse();
+        data["main"] = {};
+        for (var i=0; i< data_tmp.length; i++) {
+            data["main"][data_tmp[i].group_id] = data_tmp[i];
+        }
+        q += 1;
+        checkLoaded();
+    });
+    for (var i=0; i < data_names.length; i++) {
+        var data_name = data_names[i];
+        $.get(data_url + data_name + ".csv", function(data, status){
+            alert(data_name);
+            data[data_name] = new CSV(data, {
+                header: ["group_id", "battle_code"]
+            }).parse();
+            q += 1;
+            checkLoaded();
+        });
+    }
+}
+
+function printBracket() {
+    mc = $("#main-container");
+    loadData();
+}
+
+$(printBracket);
